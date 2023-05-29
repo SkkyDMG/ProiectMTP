4ab4198e316dfc6f4b4a18a5d9a0101f41b64f95
Lascu Teodor <93092570+SkkyDMG@users.noreply.github.com>
2023-05-30 01:19:56 +0300

    Rework Validare

---
+++ ProiectMTP/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ProiectMTP
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boxNume = new System.Windows.Forms.TextBox();
+            this.labelNume = new System.Windows.Forms.Label();
+            this.labelParola = new System.Windows.Forms.Label();
+            this.boxParola = new System.Windows.Forms.TextBox();
+            this.btnAutentificare = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // boxNume
+            // 
+            this.boxNume.Location = new System.Drawing.Point(322, 115);
+            this.boxNume.Name = "boxNume";
+            this.boxNume.Size = new System.Drawing.Size(190, 20);
+            this.boxNume.TabIndex = 0;
+            // 
+            // labelNume
+            // 
+            this.labelNume.AutoSize = true;
+            this.labelNume.Location = new System.Drawing.Point(235, 122);
+            this.labelNume.Name = "labelNume";
+            this.labelNume.Size = new System.Drawing.Size(81, 13);
+            this.labelNume.TabIndex = 1;
+            this.labelNume.Text = "Nume Utilizator:";
+            // 
+            // labelParola
+            // 
+            this.labelParola.AutoSize = true;
+            this.labelParola.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.labelParola.Location = new System.Drawing.Point(276, 161);
+            this.labelParola.Name = "labelParola";
+            this.labelParola.Size = new System.Drawing.Size(40, 13);
+            this.labelParola.TabIndex = 2;
+            this.labelParola.Text = "Parola:";
+            // 
+            // boxParola
+            // 
+            this.boxParola.Location = new System.Drawing.Point(322, 154);
+            this.boxParola.Name = "boxParola";
+            this.boxParola.PasswordChar = '#';
+            this.boxParola.Size = new System.Drawing.Size(190, 20);
+            this.boxParola.TabIndex = 3;
+            // 
+            // btnAutentificare
+            // 
+            this.btnAutentificare.Location = new System.Drawing.Point(310, 204);
+            this.btnAutentificare.Name = "btnAutentificare";
+            this.btnAutentificare.Size = new System.Drawing.Size(218, 81);
+            this.btnAutentificare.TabIndex = 4;
+            this.btnAutentificare.Text = "Autentificare";
+            this.btnAutentificare.UseVisualStyleBackColor = true;
+            this.btnAutentificare.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAutentificare);
+            this.Controls.Add(this.boxParola);
+            this.Controls.Add(this.labelParola);
+            this.Controls.Add(this.labelNume);
+            this.Controls.Add(this.boxNume);
+            this.Name = "LoginForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox boxNume;
+        private System.Windows.Forms.Label labelNume;
+        private System.Windows.Forms.Label labelParola;
+        private System.Windows.Forms.TextBox boxParola;
+        private System.Windows.Forms.Button btnAutentificare;
+    }
+}
+
---
+++ ProiectMTP/MainForm.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace ProiectMTP
+{ 
+public partial class MainForm : Form
+{
+    private const string usersFilePath = "Utilizatori.xml";
+    private List<User> users;
+
+    public MainForm()
+    {
+        InitializeComponent();
+
+        // Inițializare utilizatori
+        users = new List<User>();
+
+        LoadUsersFromXml();
+
+        // Afișare utilizatori în DataGridView
+        RefreshUserGrid();
+    }
+
+    private void LoadUsersFromXml()
+    {
+        if (File.Exists(usersFilePath))
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(usersFilePath);
+
+            XmlNodeList userNodes = xmlDoc.SelectNodes("//Utilizator");
+
+            foreach (XmlNode userNode in userNodes)
+            {
+                    string numeUtilizator = userNode.SelectSingleNode("numeUtilizator").InnerText;
+                    string parolaUtilizator = userNode.SelectSingleNode("parolaUtilizator").InnerText;
+
+                    User user = new User(numeUtilizator, parolaUtilizator);
+
+
+
+                    users.Add(user);
+            }
+        }
+    }
+
+    private void RefreshUserGrid()
+    {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = users;
+            /* dataGridView1.SuspendLayout();
+
+             // Golire DataGridView
+             dataGridView1.Rows.Clear();
+
+             // Adăugare utilizatori în DataGridView
+             foreach (User user in users)
+             {
+                 dataGridView1.Rows.Add(user.NumeUtilizator, user.ParolaUtilizator);
+             }
+
+             dataGridView1.ResumeLayout();*/
+        }
+
+        private void SaveUsersToXml()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+
+            XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            xmlDoc.AppendChild(xmlDeclaration);
+
+            XmlNode rootNode = xmlDoc.CreateElement("Utilizatori");
+            xmlDoc.AppendChild(rootNode);
+
+            foreach (User user in users)
+            {
+                XmlNode userNode = xmlDoc.CreateElement("Utilizator");
+
+                XmlNode usernameNode = xmlDoc.CreateElement("numeUtilizator");
+                usernameNode.InnerText = user.NumeUtilizator;
+                userNode.AppendChild(usernameNode);
+
+                XmlNode passwordNode = xmlDoc.CreateElement("parolaUtilizator");
+                passwordNode.InnerText = user.ParolaUtilizator;
+                userNode.AppendChild(passwordNode);
+
+                rootNode.AppendChild(userNode);
+            }
+
+            xmlDoc.Save(usersFilePath);
+        }
+
+
+        private void btnCautare_Click(object sender, EventArgs e)
+    {
+        string searchTerm = boxCautare.Text;
+
+        // Filtrare utilizatori după criteriul de căutare
+        List<User> filteredUsers = users.Where(u => u.NumeUtilizator.Contains(searchTerm)).ToList();
+
+        dataGridView1.DataSource = null;
+        dataGridView1.DataSource = filteredUsers;
+
+            boxCautare.Text = string.Empty;
+        }
+
+    private void btnStergere_Click(object sender, EventArgs e)
+    {
+            string username=boxStergere.Text;
+        //if (dataGridView1.SelectedRows.Count > 0)
+        //{
+                // Obținerea utilizatorului selectat
+                User userToDelete = users.FirstOrDefault(u => u.NumeUtilizator == username);
+                //User selectedUser = dataGridView1.SelectedRows[0].DataBoundItem as User;
+
+            //string numeUtilizator = userToDelete.NumeUtilizator;
+
+            if(userToDelete !=null)
+                {
+                    // Ștergere utilizator din lista de utilizatori
+                    users.Remove(userToDelete);
+
+                    SaveUsersToXml();
+
+                    RefreshUserGrid();
+
+                    boxStergere.Text = string.Empty;
+
+                    MessageBox.Show("Utilizatorul a fost șters cu succes!");
+                }
+                else
+                    MessageBox.Show("Utilizatorul nu a fost găsit în listă!");
+
+
+
+            //}
+    }
+
+    
+
+        private void btnValidare_Click(object sender, EventArgs e)
+        {
+            string username = boxValidare1.Text;
+            string password = boxValidare2.Text;
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Validare reușită! Utilizatorul și parola sunt introduse corect.");
+            }
+            else
+            {
+                MessageBox.Show("Validare eșuată! Vă rugăm să completați toate câmpurile.");
+            }
+        }
+
+        private void btnAdaugare_Click(object sender, EventArgs e)
+    {
+        string username = boxNewUtilizator.Text;
+        string password = boxNewParola.Text;
+
+        if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+        {
+            User newUser = new User(username, password);
+
+
+                users.Add(newUser);
+
+            SaveUsersToXml();
+
+            RefreshUserGrid();
+
+                // Resetare câmpuri
+                boxNewUtilizator.Text = string.Empty;
+                boxNewParola.Text = string.Empty;
+
+            MessageBox.Show("Utilizator adăugat cu succes!");
+        }
+        else
+        {
+            MessageBox.Show("Introduceți un nume de utilizator și o parolă valide!");
+        }
+    }
+
+        private void boxNewParola_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class User
+    {
+        public string NumeUtilizator { get; set; }
+        public string ParolaUtilizator { get; set; }
+
+        public User(string numeUtilizator, string parolaUtilizator)
+        {
+            NumeUtilizator = numeUtilizator;
+            ParolaUtilizator = parolaUtilizator;
+        }
+    }
+
+}
